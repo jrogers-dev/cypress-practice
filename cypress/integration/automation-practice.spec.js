7dfad2abb4eb567464f0b3e599fcd1933f69ef7e
Mimetic <jrogers.dev@gmail.com>
2022-02-27 21:21:21 -0800

    Added several test cases to automation-practice.spec.js

--- cypress/integration/automation-practice.spec.js
+++ cypress/integration/automation-practice.spec.js
@@ -8,17 +8,48 @@ describe("Rahul Shetty automation practice test suite", () => {
       cy.wait(1000)
     })
 
-    //Radio button
+    /*Radio button
     it("Can locate three radio buttons", () => {
         cy.get(".radioButton").should("have.length", 3)
     })
 
+    it("Can check each radio button, but not more than one at a time", () => {
+      cy.get("input[value='radio1']").check().should("be.checked")
+      cy.get("input[value='radio2']").check().should("be.checked")
+      cy.get("input[value='radio3']").check().should("be.checked")
+      cy.get("input[value='radio1']").should("not.be.checked")
+      cy.get("input[value='radio2']").should("not.be.checked")
+    })
+    
     //Dynamic Dropdown
-
+    it("Can type into the dynamic selection box and select a value", () => {
+      cy.get("#autocomplete").type("ind")
+      cy.get("#ui-id-1 .ui-menu-item").each(($el) => {
+          if($el.text() === "India") {
+            $el.trigger("click")
+          }
+      })
+      cy.get("#autocomplete").should("contain.value", "India")
+    })
+    
     //Static Dropdown
+    it("Can select an option from the static dropdown menu", () => {
+      cy.get('#dropdown-class-example').select("option2")
+      cy.get('#dropdown-class-example').should("have.value", "option2")
+      cy.get('#dropdown-class-example').should("not.have.value", "option1")
+    })
+    */
 
     //Checkboxes
-
+    it("Can check and uncheck any of the three checkboxes", () => {
+      cy.get('#checkBoxOption1').check().should("be.checked")
+      cy.get('#checkBoxOption2').check().should("be.checked")
+      cy.get('#checkBoxOption3').check().should("be.checked")
+      cy.get('#checkBoxOption1').uncheck().should("not.be.checked")
+      cy.get('#checkBoxOption2').uncheck().should("not.be.checked")
+      cy.get('#checkBoxOption3').uncheck().should("not.be.checked")
+    })
+    
     //Tables
 
     //Tables: Fixed header
